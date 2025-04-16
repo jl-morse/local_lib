213e575f33d78de15d162dd42342f9f029648a2f
jl-morse <morse.jacob@gmail.com>
2025-04-16 19:22:04 -0400

    Update admin.py

--- catalog/admin.py
+++ catalog/admin.py
@@ -6,7 +6,6 @@ from .models import Author, Genre, Book, BookInstance, Language
 admin.site.register(Book)
 admin.site.register(Author)
 admin.site.register(Genre)
-admin.site.register(BookInstance)
 admin.site.register(Language)
 
 @admin.register(BookInstance)
