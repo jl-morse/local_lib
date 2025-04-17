93375c8e05c6fa14dbbe364c8d24f717000189b9
jl-morse <morse.jacob@gmail.com>
2025-04-16 21:58:44 -0400

    idk anymore

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-release: python manage.py migrate
 web: gunicorn locallibrary.wsgi
+release: python manage.py migrate
--- manage.py
+++ manage.py
@@ -12,11 +12,9 @@ def main():
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
+            "available on your PYTHONPATH environment variable?"
         ) from exc
     execute_from_command_line(sys.argv)
-
 
 if __name__ == '__main__':
     main()
