5b2b89383fb13b7ba573db7e3cbb0773abc86b0e
jl-morse <morse.jacob@gmail.com>
2025-04-16 21:42:15 -0400

    Update settings.py

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -36,7 +36,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h*f)zm)c=6j7#(%3*i4)1cba4^*uw#!+d+0hactno%#)+0e&-o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
